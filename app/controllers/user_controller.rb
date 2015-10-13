c5834fe99a641099c46ff64b259aa2c916304057
ZhuNian <zhunianhhxy@163.com>
2015-10-13 21:54:26 +0800

    使用render响应请求

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,14 @@
 class UserController < ApplicationController
   def show
+    # 渲染页面
+    # render 'show'
+    # render action: 'show'
+    # render template: 'user/show'
+
+    # render nothing: true
+
+    # 渲染文本/JSON
+    # render plain: "OK"
+    # render json: {status:'OK'}
   end
 end
