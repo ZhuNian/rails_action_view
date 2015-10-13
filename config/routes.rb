add6650f5c4ecfc58f5e832d55ee3f1173507630
ZhuNian <zhunianhhxy@163.com>
2015-10-13 22:39:48 +0800

    redirect or render

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'user/show' => 'user#show'
-  get 'user/add'
+  get 'user/add' => 'user#add'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
